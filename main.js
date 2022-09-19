a8778aa37c9f0c014361516d84722fd45c0389e0
Matthew <matthewgonzalez2324@gmail.com>
2022-09-19 14:43:25 -0700

    Define gameboard and player logic

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+const Gameboard = (() => {
+    let boardArray = ["", "", "", "", "", "", "", "", ""];
+
+    const setField = (index, symbol) => {
+        if (index > boardArray.length) return;
+        boardArray[index] = symbol;
+    };
+
+    const getField = (index) => {
+        if (index > boardArray.length) return;
+        return boardArray[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < boardArray.length; i++){
+            boardArray[i] = "";
+        }
+    };
+
+    return {
+        setField,
+        getField,
+        reset
+    };
+
+})();
+
+const Player = (symbol) => {
+    this.symbol = symbol;
+
+    const getSymbol = () => {
+        return symbol;
+    };
+
+    return { 
+        getSymbol 
+    };
+};
+
 
