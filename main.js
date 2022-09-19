6fe478e7551df0223cc346e640f125ed05fd33f1
Matthew <matthewgonzalez2324@gmail.com>
2022-09-19 15:03:26 -0700

    Define gameController logic

--- main.js
+++ main.js
@@ -37,4 +37,64 @@ const Player = (symbol) => {
     };
 };
 
+const GameController = (() => {
+    const playerX = Player("X");
+    const playerO = Player("O");
+    let round = 1;
+    let isOver = false;
+
+    const playRound = (fieldIndex) => {
+        Gameboard.setField(fieldIndex, getCurrentPlayerSign());
+        if (checkWinner(fieldIndex)){
+            isOver = true;
+            return;
+        }
+        if (round === 9){
+            isOver = true;
+            return;
+        }
+        round++;
+    };
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+    };
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        
+        return winConditions
+            .filter((combination) => combination.includes(fieldIndex))
+            .some((possibleCombination) =>
+             possibleCombination.every(
+                (index) => Gameboard.getField(index) === getCurrentPlayerSign()
+             )
+            );
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
+
+    return {
+        playRound,
+        getIsOver,
+        reset
+    };
+})();
+
 
